64c177c6ca01eb629502a0d1d2680d040b248a20
riches2020cn <141565770+riches2020cn@users.noreply.github.com>
2023-11-07 21:32:18 +0800

    Create diy-part3.sh

---
+++ diy-part3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+
+#3. 修改默认皮肤
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon-mod/g' feeds/luci/collections/luci/Makefile
+
+#4. 修改 x86 内核版本
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
