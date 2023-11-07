1b096a049d1dff2041203a82d1b3e864e502bd22
riches2020cn <141565770+riches2020cn@users.noreply.github.com>
2023-11-07 14:57:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,15 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+
+#3. 修改默认皮肤
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon-mod/g' feeds/luci/collections/luci/Makefile
+
+#4. 修改 x86 内核版本
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+
